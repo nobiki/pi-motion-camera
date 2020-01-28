0b8b2f2772b560f65f8c3f67ee5a67629a182f0b
Naoaki Obiki <obiki.wk@gmail.com>
2020-01-29 01:20:55 +0900

    aaa

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-# sudo apt update
-# sudo apt install -y motion
+sudo apt update
+sudo apt install -y python-picamera motion autoconf automake build-essential pkgconf libtool libzip-dev libjpeg-dev git libavformat-dev libavcodec-dev libavutil-dev libswscale-dev libavdevice-dev libwebp-dev gettext libmicrohttpd-dev
+
+pip install slacker
 
 sudo ln -s ${PWD}/motion-mmalcam-both.conf /etc/motion/motion-mmalcam-both.conf
 sudo ln -s ${PWD}/pi-motion-camera.service /etc/systemd/system/pi-motion-camera.service
